86a18792b6b0d7521cb20bf1732b4570c7a39d0d
D-Jordy <kaaljordy@gmail.com>
2024-09-11 15:44:52 +0200

    Initial commit

---
+++ TestModule/src/TestClass.java
@@ -0,0 +1,16 @@
+import processing.core.PApplet;
+
+public class TestClass extends PApplet {
+    public static void main(String[] args) {
+        PApplet.main("TestClass");
+    }
+
+    public void settings() {
+    }
+
+    public void setup() {
+    }
+
+    public void draw() {
+    }
+}
