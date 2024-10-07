ee103c8770567ee41c989ffd2bb329a0cae0eef7
D-Jordy <kaaljordy@gmail.com>
2024-10-07 13:06:32 +0200

    Add yeager via Maven

---
+++ PVP Snake/src/main/java/org/snake/SnakePVP.java
@@ -0,0 +1,19 @@
+package org.snake;
+
+import com.github.hanyaeger.api.YaegerGame;
+
+public class SnakePVP extends YaegerGame {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    @Override
+    public void setupGame() {
+
+    }
+
+    @Override
+    public void setupScenes() {
+
+    }
+}
